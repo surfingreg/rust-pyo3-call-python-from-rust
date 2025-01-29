db5ec72ee3d7238f3f2359cab2c7078176d689d2
gp <github@swimr.com>
2025-01-29 12:17:55 -0800

    readme

--- readme.md
+++ readme.md
@@ -2,9 +2,15 @@
 A hello-world for pyo3. 
 
 ## Run Python from Rust
+> cargo fmt
+> cargo clippy
+> cargo vet
+> cargo audit (see .sh)
 > cargo run
 
 ## Reference
 - https://pyo3.rs/v0.23.4/
 - https://pyo3.rs/v0.23.4/python-from-rust.html
+- https://docs.rs/pyo3/latest/pyo3/struct.Bound.html
+- https://pyo3.rs/v0.23.4/migration.html?highlight=pybytes#to-python-conversions-changed-for-byte-collections-vec-u8-n-and-smallvec
 
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 //! main.rs
 //! 
 //! ref: https://pyo3.rs/v0.23.4/
+//! GIL = global interpreter lock
 //! 
 
 
@@ -14,6 +15,14 @@ use pyo3::ffi::c_str;
 /// use python from rust
 fn main() -> PyResult<()>{
 
+
+    // Acquires the global interpreter lock, allowing access to the Python interpreter. The provided 
+    // closure F will be executed with the acquired Python marker token.
+    // 
+    // If the auto-initialize feature is enabled and the Python runtime is not already initialized, 
+    // this function will initialize it. See prepare_freethreaded_python for details.
+    //
+    // https://docs.rs/pyo3/latest/pyo3/marker/struct.Python.html#method.with_gil
     Python::with_gil(|py| {
     
         println!("running python stuff...");
@@ -21,8 +30,8 @@ fn main() -> PyResult<()>{
         // Use python to get the system version:
         // >>> sys.version
         // ie. version: 3.13.1 (main, Dec  3 2024, 17:59:52) [Clang 16.0.0 (clang-1600.0.26.4)]
-        let sys = py.import("sys")?;
-        let version: String = sys.getattr("version")?.extract()?;
+        let bound: Bound<'_, PyModule> = py.import("sys")?;
+        let version: String = bound.getattr("version")?.extract()?;
         println!("version: {}", version);
 
         // >>> os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'
@@ -30,6 +39,16 @@ fn main() -> PyResult<()>{
         let code = c_str!("os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'");
         let user: String = py.eval(code, None, Some(&locals))?.extract()?;
         println!("user: {}", user);
+
+
+        // compare Python bytes to a Rust [u8] slice
+        // https://docs.rs/pyo3/latest/pyo3/types/struct.PyBytes.html#equality
+        let py_bytes = pyo3::types::PyBytes::new(py, b"wassup".as_slice());
+        assert_eq!(py_bytes, b"wassup".as_slice());
+
+
+
+
 
         Ok(())
     })
